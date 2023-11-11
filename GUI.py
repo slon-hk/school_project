0ec96a287510ed1b6fe010406d6c7a31a5e04a5a
slon_hk <54885565+slon-hk@users.noreply.github.com>
2023-11-11 19:15:50 +0300

    v.0.1

---
+++ GUI.py
@@ -0,0 +1,48 @@
+﻿import customtkinter as ctk
+import tkinter
+from tkinter import ttk
+from PIL import ImageTk, Image
+
+
+from math_func_3d import main
+
+
+# Pink Theme  #ff6fff
+
+
+class Bok_button(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.button_instrucia = ctk.CTkButton(self, text="Инструкция")
+        self.button_instrucia.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.button_parametr = ctk.CTkButton(self, text="Параметры")
+        self.button_parametr.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.button_funcia = ctk.CTkButton(self, text="Функция")
+        self.button_funcia.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="w")
+
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("project")
+        self.geometry("1600x900")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.bok_button = Bok_button(self)
+        self.bok_button.grid(row=0, column=0, padx=5, pady=(10, 0), sticky="nsw", rowspan=4)
+
+        self.slider = ctk.CTkSlider(self, height=30, width=1740)
+        self.slider.grid(row=2, column=1, padx=5, pady=10, sticky="ew")
+
+        self.button = ctk.CTkButton(self, text="Построить", width=1740, height=50)
+        self.button.grid(row=3, column=1, padx=5, pady=10)
+
+    def button_callback(self):
+        print("button pressed")
+
+
+app = App()
+app.mainloop()
---
+++ math_func_3d.py
@@ -0,0 +1,61 @@
+import sympy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+toch = 0
+symbol_x = sp.Symbol("x")
+symbol_y = sp.Symbol("y")
+
+
+def main(function, tochnost):
+    a_value = -10
+    b_value = 10
+    global toch
+    toch = tochnost
+
+    if "x" and not "y" in function:
+        plot_2d_function(function, a_value, b_value)
+    elif "x" and "y" in function:
+        plot_3d_function(function, a_value, b_value)
+    else:
+        print("You must enter a function in terms of x and/or y")
+
+
+def get_vector(a, b):
+    return np.arange(a, b + 1, toch)
+
+
+def plot_2d_function(function, a, b):
+    # Create the sympy function f(x)
+    f_x = sp.sympify(function)
+
+    # Create domain and image
+    domain_x = get_vector(a, b)
+    image = [f_x.subs(symbol_x, value) for value in domain_x]
+
+    # Plot the 2D function graph
+    fig = plt.figure(figsize=(10, 10))
+    plt.plot(domain_x, image)
+    plt.show()
+
+
+def plot_3d_function(function, a, b):
+    # Create sympy function f(x, y)
+    f_xy = sp.lambdify((symbol_x, symbol_y), sp.sympify(function))
+
+    # Create domains and image
+    domain_x = get_vector(a, b)
+    domain_y = get_vector(a, b)
+    domain_x, domain_y = np.meshgrid(domain_x, domain_y)
+    image = f_xy(domain_x, domain_y)
+
+    # Plot the 3D function graph
+    fig = plt.figure(figsize=(10, 10))
+    ax = plt.axes(projection="3d")
+    ax.plot_surface(domain_x, domain_y, image, rstride=1, cstride=1, cmap="viridis")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main("sin(x)+cos(y)", 0.5)
